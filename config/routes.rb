76d12291d7be919ed47d617106e82ddb09b1912d
AKRn31 <177292192+AKRn31@users.noreply.github.com>
2024-09-14 17:46:20 +0000

    Bookersを作制しよう/show.edit.updateページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   post 'books'=>'books#create'
   get 'books'=> 'books#index'
   get 'books/:id'=>'books#show'
-  get 'books/edit'
-  get 'books/destroy'
+  get 'books/:id'=>'books#edit'
+  patch 'books/:id'=>'books#update'
+  delete 'books'=>'books#destroy'
   get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
