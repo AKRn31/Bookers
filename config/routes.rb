2d638931e2b62156326330daa72557baa6c4cf38
AKRn31 <177292192+AKRn31@users.noreply.github.com>
2024-09-15 03:00:05 +0000

    destroyの作成（メンターさんに聞いて）

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   root "books#index"
   resources :books
- 
+
   get 'books/new'
   post 'books'=>'books#create'
-  get 'books'=> 'books#index'
   get 'books/:id'=>'books#show'
   get 'books/:id'=>'books#edit'
   patch 'books/:id'=>'books#update'
-  delete 'books'=>'books#destroy'
   get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
