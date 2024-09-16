7fd0c7c83e09cbb283590102f4dd967db561cb28
AKRn31 <177292192+AKRn31@users.noreply.github.com>
2024-09-16 15:42:44 +0000

    バリデーションとフラッシュメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,8 @@ class BooksController < ApplicationController
   end
 
   def create
-    @books = Book.all
-    @book = Book.new(books_params)
+    @book = Book.all
+    @books= Book.new(books_params)
     if @book.save
       flash[:notice]= "Book was successfully created."
       redirect_to book_path(@book.id)
@@ -29,9 +29,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book=Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book=Book.find(params[:id])
+    if @book.update(books_params)
+    flash[:notice]= "Book was successfully created."
+    redirect_to book_path(@book)
+    else 
+      render:edit
+    end
   end
 
   def destroy
